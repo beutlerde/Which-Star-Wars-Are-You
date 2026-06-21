b6d9c00016a9e8ab661203a4b89b310b5d9ef6f8
beutlerde <beutlerde9@gmail.com>
2026-06-21 00:04:23 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
    Bump the version number to force all devices to reload.
    ============================================================ */
 
-var CACHE_NAME = 'star-wars-v5';
+var CACHE_NAME = 'star-wars-v6';
 
 /* Core shell — always cached */
 var CORE_FILES = [
